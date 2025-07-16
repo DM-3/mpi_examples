2d09807e8a57cc5dc236197df81a3900ffb47d9c
DM-3 <m.werner.0@protonmail.com>
2025-07-16 16:50:43 +0200

    Distribute board update computation with mpi and demonstrate strong scaling for large boards

--- game_of_life.py
+++ game_of_life.py
@@ -1,3 +1,7 @@
+# usage example:
+# mpiexec -n 4 python3 game_of_life.py --N 1000 --t_steps 1000 --show --probability 0.3
+
+from mpi4py import MPI
 import numpy as np
 import cv2 as cv
 import time
@@ -75,7 +79,10 @@ def read_parameters() -> tuple[int, int, float, bool]:
         return 100, 100, 0.4, True
 
 
-def create_random_board(size: tuple[int, int], probability: float) -> np.ndarray[np.uint8]:
+def create_random_board(
+        size: tuple[int, int], 
+        probability: float, 
+        seed: int) -> np.ndarray[np.uint8]:
     """
     Initialize the board randomly with given probability of alive cells.
 
@@ -85,24 +92,37 @@ def create_random_board(size: tuple[int, int], probability: float) -> np.ndarray
         board height and width
 
     probability : float
-        Probability of a cell being alive initially
-    """
-    np.random.seed(0)
-    return np.where(np.random.rand(*size) > probability, np.uint8(1), np.uint8(0))
-
-
-def update_step(source: np.ndarray[np.uint8], target: np.ndarray[np.uint8]):
+        probability of a cell being alive initially
     
-    # copy torus padding
-    source[0,:]  = source[-2,:]
-    source[-1,:] = source[1,:]
-    source[:,0]  = source[:,-2]
+    seed : int
+        seed for random number generation
+    """
+    np.random.seed(seed)
+    return np.where(np.random.rand(*size) < probability, np.uint8(1), np.uint8(0))
+
+
+def update_step(
+        source: np.ndarray[np.uint8], 
+        target: np.ndarray[np.uint8],
+        comm,
+        rank: int,
+        n_ranks: int):
+    
+    # copy local torus padding columns
+    source[:,0] = source[:,-2]
     source[:,-1] = source[:,1]
 
-    source[0,0]   = source[-2,-2]
-    source[0,-1]  = source[-2,1]
-    source[-1,0]  = source[1,-2]
-    source[-1,-1] = source[1,1]
+    # communicate global torus padding rows
+    # cycle up in rank
+    comm.Barrier()
+    dst = (rank + 1 + n_ranks) % n_ranks
+    src = (rank - 1 + n_ranks) % n_ranks
+    source[0,:] = comm.sendrecv(source[-2,:], dest=dst, source=src)
+    # cycle down in rank
+    comm.Barrier()
+    dst = (rank - 1 + n_ranks) % n_ranks
+    src = (rank + 1 + n_ranks) % n_ranks
+    source[-1,:] = comm.sendrecv(source[1,:], dest=dst, source=src)
 
     # count neighbors for all pixels
     w = np.asarray([[1,1,1],
@@ -123,27 +143,32 @@ def main():
     print(f"Game of Life with N = {N}, {t_steps} timesteps "
           f"and initial probability = {probability}")
 
+    # initialize mpi
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    n_ranks = comm.Get_size()
+
     # initialize board and temporary board for computations
     padding = 2
     WIDTH = N + padding
-    HEIGHT = N + padding
-    board = create_random_board((HEIGHT, WIDTH), probability)
+    HEIGHT = N // n_ranks + padding
+    board = create_random_board((HEIGHT, WIDTH), probability, rank)
     t_board = np.zeros_like(board)
 
     # update loop
     if show:
         while cv.waitKey(1) != 27:
-            cv.imshow('board', cv.resize(board[1:-1,1:-1] * 255, 
-                                (900, 900), 
+            cv.imshow(f'{rank} board', cv.resize(board[1:-1,1:-1] * 255, 
+                                (900, 900 // n_ranks), 
                                 interpolation=cv.INTER_NEAREST))
-            update_step(board, t_board)         # update board
-            board, t_board = t_board, board     # swap with temporary board
+            update_step(board, t_board, comm, rank, n_ranks)    # update board
+            board, t_board = t_board, board                     # swap with temporary board
     else:
         start = time.time()
 
         for _ in range(t_steps):
-            update_step(board, t_board)         # update board
-            board, t_board = t_board, board     # swap with temporary board
+            update_step(board, t_board, comm, rank, n_ranks)    # update board
+            board, t_board = t_board, board                     # swap with temporary board
 
         end = time.time()
         print(f'computation took {end-start:.6f} seconds')
